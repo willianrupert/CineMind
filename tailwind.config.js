1088e8be29369a461d33ee6f42ce18f667fd97db
Willian Rupert <willianrupert@gmail.com>
2025-10-22 11:28:03 -0300

    Fix content path in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // <-- A linha importante
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
