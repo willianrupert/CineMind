8a6011e3a5ed1426e68b6878fb0c4af4b7766110
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-03 14:01:19 -0300

    test: cria teste de navegação das rotas

--- frontend/test/CineMind.test.tsx
+++ frontend/test/CineMind.test.tsx
@@ -1,3 +1,27 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import CineMind from "../src/CineMind";
+import { MemoryRouter } from "react-router-dom";
+
 describe("Componente CineMind", () => {
-  test("Navegação das rotas", () => {});
+  // mantenha esses dados consistentes com os que estão no componente
+  const routesToTestIDs = new Map<string, string>([
+    ["/", "landing-page"],
+    ["/login", "login-page"],
+    ["/signup", "signup-page"],
+    ["/questionnaire", "questionnaire-page"],
+    ["/home", "home-page"],
+    ["/profile", "profile-page"]
+  ]);
+
+  test("Navegação das rotas", async () => {
+    for (const [route, testID] of routesToTestIDs.entries()) {
+      render(
+        <MemoryRouter initialEntries={[route]}>
+          <CineMind />
+        </MemoryRouter>
+      );
+
+      expect(await screen.findByTestId(testID)).toBeInTheDocument();
+    }
+  });
 });
