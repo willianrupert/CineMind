1bfa5afcf70ccfe1f521c1eae3e46051e67610da
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-04 15:27:12 -0300

    feat(front): cria componente `SVGIcon`

---
+++ frontend/src/assets/SVGIcon.tsx
@@ -0,0 +1,45 @@
+/**
+ * Tipo que define as propriedades esperadas por cada ícone SVG.
+ */
+export interface IconProps {
+  className?: string;
+  viewBox?: string;
+  onClick?: (event: React.SyntheticEvent) => void;
+}
+
+/** Tipo para uso interno. */
+interface SVGIconProps {
+  className: string;
+  viewBox: string;
+  onClick: (event: React.SyntheticEvent) => void;
+  children: React.JSX.Element | React.JSX.Element[];
+}
+
+/**
+ * Representa um ícone genérico de SVG interativo.
+ *
+ * Não é recomendado usar essa função em nenhuma página diretamente.
+ * Esse componente apenas facilita o processo de criação de assets.
+ *
+ * @param className As classes do componente.
+ * @param viewBox O atributo {@link React.SVGAttributes<SVGSVGElement>.viewBox} do SVG.
+ * @param onClick Uma função disparada quando o usuário clica no ícone.
+ * @param children Elementos-filho do SVG, como {@link React.JSX.IntrinsicElements.path \<path \/\>}.
+ */
+export default function SVGIcon({
+  className,
+  viewBox,
+  onClick,
+  children
+}: SVGIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={viewBox}
+      className={className}
+      onClick={onClick}
+    >
+      {children}
+    </svg>
+  );
+}
