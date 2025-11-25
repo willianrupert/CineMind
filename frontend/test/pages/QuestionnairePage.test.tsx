7ada01c9ca3851cd39b65597ec0b4df33ee8fbb6
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-25 15:26:40 -0300

    test(front): arquivo de teste para a página  `Questionnaire` preliminar

---
+++ frontend/test/pages/QuestionnairePage.test.tsx
@@ -0,0 +1,32 @@
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import QuestionnairePage from "../../src/pages/QuestionnairePage";
+import { render } from "@testing-library/react";
+
+describe("Componente Questionnaire", () => {
+  beforeEach(async () => {
+    render(
+      <MemoryRouter initialEntries={["/questionnaire"]}>
+        <Routes>
+          <Route
+            path="/questionnaire"
+            element={
+              <div data-testid="questionnaire-page">
+                <QuestionnairePage />
+              </div>
+            }
+          />
+          <Route
+            path="/home"
+            element={<div data-testid="home-page" />}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+  });
+
+  test("Estrutura da página", async () => {});
+
+  test("Cenário: marcar respostas", async () => {});
+
+  test("Cenário: tentar submeter questionário incompleto", async () => {});
+});
