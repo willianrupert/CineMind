384355d22041468123d7b91dc8a324bb09683d0f
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-26 17:04:18 -0300

    refactor(front): torna `NavBar` um componente dinâmico

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,56 +1,68 @@
+import type { JSX } from "react";
 import { useNavigate } from "react-router-dom";
-import HomeIcon from "../assets/HomeIcon";
-import LogoutIcon from "../assets/LogoutIcon";
-import ProfileIcon from "../assets/ProfileIcon";
 import { StorageKeys } from "../utils/constants";
+import LogoutIcon from "../assets/LogoutIcon";
+import HomeIcon from "../assets/HomeIcon";
+import ProfileIcon from "../assets/ProfileIcon";
 
-interface NavbarProps {
+interface NavBarProps {
+  className?: string;
+  icons?: () => JSX.Element[];
   selectedIcon: number;
 }
 
-export default function Navbar({ selectedIcon = 0 }: NavbarProps) {
+export const DEFAULT_NAVBAR_ICONS = () => {
   const navigate = useNavigate();
   const goToLoginPage = () => navigate("/login");
   const goToHomePage = () => navigate("/home");
   const goToProfilePage = () => navigate("/profile");
-
   const logout = () => {
     localStorage.removeItem(StorageKeys.ACCESS_TOKEN);
     goToLoginPage();
   };
 
+  const iconProperties =
+    "size-12 rounded-full fill-none stroke-cinemind-white stroke-1 bg-cinemind-dark outline-2 outline-cinemind-light";
+  const iconViewBox = "-4 -4 32 32";
+
+  return [
+    <LogoutIcon
+      className={iconProperties}
+      viewBox={iconViewBox}
+      onClick={logout}
+    />,
+
+    <HomeIcon
+      className={iconProperties}
+      viewBox={iconViewBox}
+      onClick={goToHomePage}
+    />,
+
+    <ProfileIcon
+      className={iconProperties}
+      viewBox={iconViewBox}
+      onClick={goToProfilePage}
+    />
+  ];
+};
+
+export default function NavBar({
+  className = "",
+  icons = () => [],
+  selectedIcon = 0
+}: NavBarProps) {
   return (
-    <div
-      className="
-        flex bottom-4 gap-8 p-2 rounded-full overflow-visible relative
-        row-start-10 row-span-1 col-start-2
-        bg-cinemind-light
-      "
-    >
-      <LogoutIcon
-        className={`
-          size-12 rounded-full fill-none stroke-cinemind-white stroke-1 
-          bg-cinemind-dark outline-2 outline-cinemind-light ${selectedIcon == 0 ? "scale-200" : "cursor-pointer"}
-        `}
-        viewBox="-4 -4 32 32"
-        onClick={logout}
-      />
-      <HomeIcon
-        className={`
-          size-12 rounded-full fill-none stroke-cinemind-white stroke-1 
-          bg-cinemind-dark outline-2 outline-cinemind-light ${selectedIcon == 1 ? "scale-200" : "cursor-pointer"}
-        `}
-        viewBox="-4 -4 32 32"
-        onClick={goToHomePage}
-      />
-      <ProfileIcon
-        className={`
-          size-12 rounded-full fill-none stroke-cinemind-white stroke-1 
-          bg-cinemind-dark outline-2 outline-cinemind-light ${selectedIcon == 2 ? "scale-200" : "cursor-pointer"}
-        `}
-        viewBox="-4 -4 32 32"
-        onClick={goToProfilePage}
-      />
+    <div className={className}>
+      {icons().map((icon, index) => {
+        return (
+          <div
+            key={index}
+            className={selectedIcon == index ? "scale-200" : "cursor-pointer"}
+          >
+            {icon}
+          </div>
+        );
+      })}
     </div>
   );
 }
